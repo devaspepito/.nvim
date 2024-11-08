4522523481d925bee4dcb6bfe2da75ba70a228e2
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-11-07 20:26:38 -0500

    Change all colorschemes to monochrome

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,8 @@
 return {
-	{
-		"sainnhe/sonokai",
-		priority = 1000,
-		name = "sonokai",
-		config = function()
-			vim.g.sonokai_transparent_background = "1"
-			vim.g.sonokai_enable_italic = "1"
-			vim.g.sonokai_style = "andromeda"
-		end,
-	},
-	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000,
-		lazy = true,
-		name = "onedark",
-	},
-	{
-		"rose-pine/neovim",
-		priority = 1000,
-		lazy = true,
-		name = "rose-pine",
-	},
-	{
-		"EdenEast/nightfox.nvim",
-		name = "nightfox",
-	},
-	{
-		"xiyaowong/transparent.nvim",
-		priority = 1000,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "nightfox",
-		},
-	},
+	"kdheepak/monochrome.nvim",
+	priority = 1000,
+	config = function()
+		vim.g.monochrome_enable_italic = "1"
+		vim.cmd.colorscheme("monochrome")
+	end,
 }
