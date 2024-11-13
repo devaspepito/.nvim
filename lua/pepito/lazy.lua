5f84566f10631e211ef64d7870f0a19b4d7dad40
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-11-13 18:24:09 -0500

    add all plugins to install

--- lua/pepito/lazy.lua
+++ lua/pepito/lazy.lua
@@ -12,8 +12,170 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		os.exit(1)
 	end
 end
-vim.opt.rtp:prepend(lazypath)
+
+opt.rtp:prepend(lazypath)
+
+g.mapleader = " "
 
 require("lazy").setup({
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+	"RRethy/nvim-base16",
+	{
+		"altermo/ultimate-autopair.nvim",
+		event = { "InsertEnter", "CmdlineEnter" },
+		branch = "v0.6",
+		opts = {},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"rose-pine/neovim",
+		name = "rosepine",
+		config = function()
+			vim.g.rosepine_enable_italic = "1"
+		end,
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"onsails/lspkind.nvim",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		after = "nvim-cmp",
+		dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"adalessa/laravel.nvim",
+		dependencies = {
+			"tpope/vim-dotenv",
+			"nvim-telescope/telescope.nvim",
+			"MunifTanjim/nui.nvim",
+			"kevinhwang91/promise-async",
+		},
+		cmd = { "Laravel" },
+		event = { "VeryLazy" },
+		opts = {},
+		config = true,
+	},
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"neovim/nvim-lspconfig",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+	},
+	{
+		"OXY2DEV/markview.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"kyazdani42/nvim-tree.lua",
+	},
+	{
+		"nvimtools/none-ls.nvim",
+	},
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = "neovim/nvim-lspconfig",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		dependencies = { { "echasnovski/mini.icons", opts = {} }, { "nvim-tree/nvim-web-devicons" } },
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+	},
+	{
+		"folke/twilight.nvim",
+	},
+	{
+		"akinsho/bufferline.nvim",
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+	},
+	{
+		"tpope/vim-dadbod",
+		"kristijanhusak/vim-dadbod-ui",
+		"kristijanhusak/vim-dadbod-completion",
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 500
+		end,
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+			},
+		},
+	},
+	{
+		"folke/zen-mode.nvim",
+	},
+	-- {
+	-- 	"xiyaowong/transparent.nvim",
+	-- },
 	{ import = "plugins" },
 })
