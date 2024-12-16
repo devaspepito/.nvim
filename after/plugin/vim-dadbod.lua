dc64609adfc0f24fb34303a7c7ad61d21a944f82
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-12-15 19:10:43 -0500

    some configs

---
+++ after/plugin/vim-dadbod.lua
@@ -0,0 +1,8 @@
+------------------------ Example ----------------------------
+
+-- vim.g.dbs = {
+	-- With SQLServer
+	-- DBname = "sqlserver://sa:StrongPassword@localhost:1433/DBname?TrustServerCertificate=yes",
+	--With sqlite
+	--With mysql
+-- }
